a8bf1f6828b7f71a191babbf1c7fc199b6b8be5b
Julien Breiner <julien@dwd.com.au>
2018-04-29 10:13:24 +0200

    restart

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,6 +1,11 @@
 import Lvl from "./level"
+import Cell from "./cell"
 
 if (document.getElementById("user-lvl")) {
     let lvl = new Lvl();
     lvl.init();
+}
+
+if (document.getElementById("cell")) {
+    let cell = new Cell();
 }
