2d82b1952b8866d58fa956c2545592e86e49ec13
Julien Breiner <julien.breiner@gmail.com>
2018-07-30 22:42:28 +0200

    draw unwalkable cells

---
+++ src/front/admin/grid/Cell.ts
@@ -0,0 +1,40 @@
+import GridAdmin from "./GridAdmin";
+
+export default class Cell extends PIXI.Container {
+
+    private bg: PIXI.Graphics;
+
+    private grid: GridAdmin
+
+    constructor(grid: GridAdmin) {
+        super()
+
+        this.grid = grid;
+
+        this.bg = new PIXI.Graphics();
+        this.bg.beginFill(0xFF0000, .5)
+        this.bg.drawRect(0, 0, 20, 20);
+        this.bg.alpha = 0;
+        this.addChild(this.bg)
+
+        const border = new PIXI.Graphics();
+        border.lineStyle(1);
+        border.drawRect(0, 0, 20, 20);
+        this.addChild(border)
+
+        this.interactive = true;
+
+        this.on("mousedown", this.onMouseDown.bind(this))
+        this.on("mouseover", this.onMouseOver.bind(this))
+    }
+
+    private onMouseDown() {
+        this.bg.alpha = this.bg.alpha === 0 ? 1 : 0;
+        this.grid.draw = this.bg.alpha;
+    }
+
+    private onMouseOver() {
+        if (isNaN(this.grid.draw)) return;
+        this.bg.alpha = this.grid.draw;
+    }
+}
--- src/front/admin/grid/GridAdmin.ts
+++ src/front/admin/grid/GridAdmin.ts
@@ -1,27 +1,58 @@
 import * as PIXI from "pixi.js"
+import Cell from "./Cell";
 
 export default class GridAdmin {
 
     public app: PIXI.Application;
+
+    private bg: PIXI.Container;
+
+    private grid: PIXI.Container;
+
+    public draw: number = NaN;
 
     constructor() {
         PIXI.utils.skipHello();
         this.app = new PIXI.Application({
             width: 560,
             height: 560,
-            transparent: false,
+            transparent: true,
             antialias: true
         })
         document.getElementById("grid-admin").appendChild(this.app.view)
 
         this.app.stage.interactive = true;
 
+        this.bg = new PIXI.Container()
+        this.app.stage.addChild(this.bg)
 
+        this.grid = new PIXI.Container()
+        this.app.stage.addChild(this.grid);
+
+        this.grid.interactive = true;
+        this.grid.on("mouseup", this.onMouseUp.bind(this))
+
+        this.drawGrid();
     }
 
     public load(url: string) {
-        console.log(url);
-        let img = PIXI.Sprite.fromImage(url)
-        this.app.stage.addChild(img)
+        this.bg.removeChildren()
+        this.bg.addChild(PIXI.Sprite.fromImage(url));
+    }
+
+    private drawGrid() {
+        let cell;
+        for (let i = 0; i < 560 / 20; i++) {
+            for (let j = 0; j < 560 / 20; j++) {
+                cell = new Cell(this);
+                cell.x = j * 20;
+                cell.y = i * 20;
+                this.grid.addChild(cell)
+            }
+        }
+    }
+
+    private onMouseUp() {
+        this.draw = NaN;
     }
 }
